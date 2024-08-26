7c748367a7ad59754e6d8b940decdb563bef04ac
Vinicius Canuto <84680268+viniciuscanutx@users.noreply.github.com>
2024-08-26 10:52:41 -0300

    Delete config/db.py

--- config/db.py
+++
@@ -1,4 +0,0 @@
-from pymongo import MongoClient
-
-conn = MongoClient("mongodb+srv://brcanuto:j0N0QLM5aKR2JPOQ@filmsanddo.kaxeh.mongodb.net/?retryWrites=true&w=majority&appName=Filmsanddo")
-db = conn.filmes
