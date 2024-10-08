e2a0e137d4cd970bb4b67f7aad5b89272c944705
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-10-08 12:44:57 -0300

    Routing problems

--- config/db.py
+++ config/db.py
@@ -3,5 +3,5 @@ from pymongo import MongoClient
 
 ca = certifi.where()
 
-conn = MongoClient("mongodb+srv://1532:WdZgzxFCpbCoQ8yl@filmsanddo.kaxeh.mongodb.net/?retryWrites=true&w=majority&appName=Filmsanddo", tlsCAFile=ca)
+conn = MongoClient("mongodb+srv://1532:VCOl0q5Nm663fsUC@filmsanddo.kaxeh.mongodb.net/?retryWrites=true&w=majority&appName=Filmsanddo", tlsCAFile=ca)
 db = conn.filmes
